d2deee5cfdde3a33544d1e3cb67bc401f07c833a
Parthiv Upadhyay <parthivu1309@gmail.com>
2026-05-15 23:54:30 +0530

    Person.cpp was writtin and constructor was declared in Person.h

--- include/users/Person.h
+++ include/users/Person.h
@@ -8,4 +8,6 @@ class Person{
     public:
         //A pure virtual function so that my class is Abstarct Base Class(ABC);
         virtual void display() = 0;
+        //declaring a constructor for the class
+        Person(int id, string name);
 };
---
+++ src/users/Person.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include "Person.h"
+using namespace std;
+
+Person :: Person(int id, string name){
+    this->id = id;
+    this->name = name;
+}
